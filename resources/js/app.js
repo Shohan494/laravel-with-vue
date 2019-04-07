5417aa3f32047b83f990455d53b7a60f7ea5f927
shohan494 <shohan12224943@gmail.com>
2019-04-07 23:32:48 +0600

    vue router with components

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,5 @@
+// app.js
+
 require('./bootstrap');
 
 window.Vue = require('vue');
@@ -7,9 +9,38 @@ Vue.use(VueRouter);
 
 import VueAxios from 'vue-axios';
 import axios from 'axios';
+
+import App from './App.vue';
 Vue.use(VueAxios, axios);
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+import HomeComponent from './components/HomeComponent.vue';
+import CreateComponent from './components/CreateComponent.vue';
+import IndexComponent from './components/IndexComponent.vue';
+import EditComponent from './components/EditComponent.vue';
 
-const router = new VueRouter({ mode: 'history'});
-const app = new Vue(Vue.util.extend({ router })).$mount('#app');
+const routes = [
+  {
+      name: 'home',
+      path: '/',
+      component: HomeComponent
+  },
+  {
+      name: 'create',
+      path: '/create',
+      component: CreateComponent
+  },
+  {
+      name: 'posts',
+      path: '/posts',
+      component: IndexComponent
+  },
+  {
+      name: 'edit',
+      path: '/edit/:id',
+      component: EditComponent
+  }
+];
+
+const router = new VueRouter({ mode: 'history', routes: routes});
+const app = new Vue(Vue.util.extend({ router }, App)).$mount('#app');
+
